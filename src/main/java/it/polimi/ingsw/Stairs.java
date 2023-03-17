6e495066516a777acc6e0744efac9da2aba3b8c4
Simone <simone.corbo@mail.polimi.it>
2023-03-17 17:32:07 +0100

    JavaDoc Stairs and Readme fix

--- src/main/java/it/polimi/ingsw/Stairs.java
+++ src/main/java/it/polimi/ingsw/Stairs.java
@@ -1,16 +1,39 @@
 package it.polimi.ingsw;
 
+/** This class implements the methods of Layout for a diagonal/stair common goal
+ *
+ */
 public class Stairs extends Layout {
+    /** this attribute specifies whether is a diagonal (true), or a stair (diagonal+lower half)
+     */
     private boolean diagonal;
+
+    /** this attribute specifies whether is a double diagonal (true) (X Shape) or a simple diagonal (false)
+     */
     private boolean doublediagonal;
 
+    /**Constructor
+     *
+     * @param width width of the
+     * @param height
+     * @param minDifferent
+     * @param maxDifferent
+     * @param occurrences
+     * @param rotate
+     * @param diagonal
+     * @param doublediagonal
+     */
     public Stairs(int width, int height, int minDifferent, int maxDifferent, int occurrences, boolean rotate, boolean diagonal, boolean doublediagonal) {
         super(width, height, minDifferent, maxDifferent, occurrences, rotate, diagonal, doublediagonal);
     }
 
+
     public boolean check(Bookshelf B) {
-        for (int i = getWidth(); i < (B.getColumns() - getWidth()); i++) {
+        int counter=0;
+
+        for (int i = getWidth()-1; i < (B.getColumns() - getWidth()); i++) {
 // Add implementation
         }
+        return false;
     }
 }
