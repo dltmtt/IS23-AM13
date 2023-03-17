75bb67de9a6d1b314bf4fc204d28f05031d75167
Matteo Delton <deltonmatteo@gmail.com>
2023-03-17 19:14:15 +0100

    Add more tests

--- src/test/java/it/polimi/ingsw/BookshelfTest.java
+++ src/test/java/it/polimi/ingsw/BookshelfTest.java
@@ -1,19 +1,50 @@
 package it.polimi.ingsw;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BookshelfTest {
-    @org.junit.jupiter.api.Test
+    @Test
     void insertAnElement() {
         Bookshelf bookshelf = new Bookshelf();
         Item item = new Item(Color.BLUE, 1);
         List<Item> items = new ArrayList<>();
         items.add(item);
-
         bookshelf.insert(0, items);
         assertEquals(bookshelf.getFreeCellsInColumn(0), 4);
+    }
+
+    @Test
+    void insertZeroElements() {
+        Bookshelf bookshelf = new Bookshelf();
+        List<Item> items = new ArrayList<>();
+        bookshelf.insert(0, items);
+        assertEquals(bookshelf.getFreeCellsInColumn(0), 5);
+    }
+
+    @Test
+    void fillColumn() {
+        Bookshelf bookshelf = new Bookshelf();
+        List<Item> items = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            items.add(new Item(Color.GREEN, 2));
+        }
+        bookshelf.insert(0, items);
+        assertEquals(bookshelf.getFreeCellsInColumn(0), 0);
+    }
+
+    @Test
+    void insertMoreElementsThanFreeCells() {
+        Bookshelf bookshelf = new Bookshelf();
+        List<Item> items = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            items.add(new Item(Color.LIGHTBLUE, 3));
+        }
+        bookshelf.insert(0, items);
+        assertEquals(bookshelf.getFreeCellsInColumn(0), 5);
     }
 }
