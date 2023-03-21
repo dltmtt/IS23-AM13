14672ac8d6aed8a5d6fca671e56352e840d73b41
Beatrice Di Raimondo Metallo <beatrice01@alice.it>
2023-03-21 16:40:46 +0100

    Add CommonGoalTest

---
+++ src/test/java/it/polimi/ingsw/CommonGoalTest.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CommonGoalTest {
+
+    @Test
+    void check() {
+        Layout s = new Stair(2, 1, 3, 3, 2, 'c', true, true);
+        CommonGoal c = new CommonGoal(3, s);
+
+
+        assertEquals(c.getScoringList().get(0), 8);
+        assertEquals(c.getScoringList().get(1), 6);
+        assertEquals(c.getScoringList().get(2), 4);
+
+
+        CommonGoal c1 = new CommonGoal(2, s);
+        assertEquals(c1.getScoringList().get(0), 8);
+        assertEquals(c1.getScoringList().get(1), 4);
+
+        CommonGoal c2 = new CommonGoal(4, s);
+        assertEquals(c2.getScoringList().get(0), 8);
+        assertEquals(c2.getScoringList().get(1), 6);
+        assertEquals(c2.getScoringList().get(2), 4);
+        // assertEquals(c.getScoringList().get(3),2);
+    }
+}
