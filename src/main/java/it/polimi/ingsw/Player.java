7b3e2d6fe134134a9e64971f01aa84afaf63cf2a
Matteo Delton <deltonmatteo@gmail.com>
2023-03-22 10:35:10 +0100

    Initial (incomplete) implementation

--- src/main/java/it/polimi/ingsw/Player.java
+++ src/main/java/it/polimi/ingsw/Player.java
@@ -1,4 +1,42 @@
 package it.polimi.ingsw;
 
+import java.util.List;
+
 public class Player {
+    private static List<CommonGoal> commonGoals;
+    private final String nickname;
+    private final int age;
+    private final boolean isFirstGame;
+    private final boolean isFirstPlayer;
+    private final boolean hasEndGameCard;
+    private Bookshelf bookshelf;
+    private PersonalGoal personalGoal;
+
+    public Player(String nickname, int age, boolean isFirstGame, boolean isFirstPlayer, boolean hasEndGameCard) {
+        this.nickname = nickname;
+        this.age = age;
+        this.isFirstGame = isFirstGame;
+        this.isFirstPlayer = isFirstPlayer;
+        this.hasEndGameCard = hasEndGameCard;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isFirstGame() {
+        return isFirstGame;
+    }
+
+    public boolean isFirstPlayer() {
+        return isFirstPlayer;
+    }
+
+    public boolean hasEndGameCard() {
+        return hasEndGameCard;
+    }
 }
