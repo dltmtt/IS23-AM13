a173d92f1748011cf158140992eb925fa1409bb6
Matteo Delton <deltonmatteo@gmail.com>
2023-03-25 11:24:58 +0100

    Add calculateScore, rearrangePickedItems, move and more

--- src/main/java/it/polimi/ingsw/Player.java
+++ src/main/java/it/polimi/ingsw/Player.java
@@ -1,23 +1,37 @@
 package it.polimi.ingsw;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+@SuppressWarnings("unused")
+
 public class Player {
-    private static List<CommonGoal> commonGoals;
+    // Maybe we don't need this, we assign the common goal points in the CommonGoal class
+    private static final CommonGoal[] commonGoals = new CommonGoal[2];
+    private static Board board;
     private final String nickname;
     private final int age;
     private final boolean isFirstGame;
     private final boolean isFirstPlayer;
     private final boolean hasEndGameCard;
-    private Bookshelf bookshelf;
+    private final int[] commonGoalPoints = new int[commonGoals.length];
+    private final Bookshelf bookshelf;
     private PersonalGoal personalGoal;
 
-    public Player(String nickname, int age, boolean isFirstGame, boolean isFirstPlayer, boolean hasEndGameCard) {
+    public Player(String nickname, int age, boolean isFirstGame, boolean isFirstPlayer, boolean hasEndGameCard, Bookshelf bookshelf, Board board) {
         this.nickname = nickname;
         this.age = age;
         this.isFirstGame = isFirstGame;
         this.isFirstPlayer = isFirstPlayer;
         this.hasEndGameCard = hasEndGameCard;
+        this.bookshelf = bookshelf;
+        Player.board = board;
+        Arrays.fill(commonGoalPoints, 0);
+    }
+
+    public static void setCommonGoal(CommonGoal commonGoal, int index) {
+        commonGoals[index] = commonGoal;
     }
 
     public String getNickname() {
@@ -42,5 +56,100 @@ public class Player {
 
     public void setPersonalGoal(PersonalGoal personalGoal) {
         this.personalGoal = personalGoal;
+    }
+
+    /**
+     * This method moves a straight line of tiles from the board to the bookshelf.
+     *
+     * @param from   the starting point of the line
+     * @param to     the ending point of the line
+     * @param column the column of the bookshelf where the tiles will be placed (starting from 0)
+     * @throws IllegalArgumentException if the line is not straight or if the selection is empty
+     */
+    public void move(IntegerPair from, IntegerPair to, int column) throws IllegalArgumentException {
+        List<IntegerPair> list = new ArrayList<>();
+        int lengthX = Math.abs(from.getX() - to.getX());
+        int lengthY = Math.abs(from.getY() - to.getY());
+        int length = Math.max(lengthX, lengthY);
+
+        if (length == 0) {
+            throw new IllegalArgumentException("You must move at least one tile.");
+        }
+
+        if (lengthX > 1 && lengthY > 1) {
+            throw new IllegalArgumentException("You can only move in a straight line.");
+        }
+
+        if (Math.abs(from.getX() - to.getX()) > Math.abs(from.getY() - to.getY())) {
+            for (int i = 0; i < length; i++) {
+                list.add(new IntegerPair(from.getX() + i, from.getY()));
+            }
+        } else {
+            for (int i = 0; i < length; i++) {
+                list.add(new IntegerPair(from.getX(), from.getY() + i));
+            }
+        }
+
+        try {
+            bookshelf.insert(column, board.pickFromBoard(list));
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * This method rearranges the picked items according to the given order.
+     * For example, if the order is [2, 0, 1], the first item will be placed in the third position,
+     * the second item will be placed in the first position and the third item will be placed in the second position.
+     *
+     * @param items the items to rearrange
+     * @param order the order in which the items will be placed
+     * @throws IllegalArgumentException  if the number of items and the number of positions are not the same or if an item is placed in two different positions
+     * @throws IndexOutOfBoundsException if the new position is out of bounds
+     */
+    public void rearrangePickedItems(List<Item> items, int[] order) throws IllegalArgumentException, IndexOutOfBoundsException {
+        if (items.size() != order.length) {
+            throw new IllegalArgumentException("The number of items and the number of positions are not the same.");
+        }
+
+        List<Item> rearrangedItems = new ArrayList<>();
+
+        for (int i : order) {
+            if (i >= items.size()) {
+                throw new IndexOutOfBoundsException("The new position is out of bounds.");
+            }
+
+            if (rearrangedItems.contains(items.get(i))) {
+                throw new IllegalArgumentException("You can't place the same item in two different positions.");
+            }
+
+            rearrangedItems.add(items.get(i));
+        }
+
+        items.clear();
+        items.addAll(rearrangedItems);
+    }
+
+    /**
+     * This method calculates the score of the player.
+     *
+     * @return the score of the player
+     */
+    public int calculateScore() {
+        int score = 0;
+
+        if (hasEndGameCard) {
+            score += 1;
+        }
+
+        // Do we really have to implement "getPoints()" in CommonGoal?
+        for (int scoring : commonGoalPoints) {
+            score += scoring;
+        }
+
+        score += personalGoal.getPoints();
+        score += bookshelf.getPoints();
+
+        return score;
     }
 }
