48faed2d13e1690370c946266bef01460174c413
Matteo Delton <deltonmatteo@gmail.com>
2023-03-25 15:23:51 +0100

    Make attributes final, simplify if, suppress unused warning
    The condition maxDifferent < 0 is always false, therefore it has been remove (with the relative error message)

--- src/main/java/it/polimi/ingsw/Layout.java
+++ src/main/java/it/polimi/ingsw/Layout.java
@@ -1,33 +1,31 @@
 package it.polimi.ingsw;
 
+@SuppressWarnings("unused")
+
 public abstract class Layout {
-    private int width;
-    private int height;
-    private int minDifferent;
-    private int maxDifferent;
-    //private char direction;
+    private final int width;
+    private final int height;
+    private final int minDifferent;
+    private final int maxDifferent;
+//    private char direction;
 
     public Layout(int width, int height, int minDifferent, int maxDifferent) {
-        if(width<=0 || height<=0 || minDifferent>maxDifferent || minDifferent<0 || maxDifferent<0){
-            String ErrorMessage="Invalid Parameters: ";
-            if(width<=0){
-                ErrorMessage+="width set to a negative value ("+width+") ";
+        if (width <= 0 || height <= 0 || minDifferent > maxDifferent || minDifferent < 0) {
+            String ErrorMessage = "Invalid Parameters: ";
+            if (width <= 0) {
+                ErrorMessage += "width set to a negative value (" + width + ") ";
             }
 
-            if(height<=0){
-                ErrorMessage+="height set to a negative value ("+height+") ";
+            if (height <= 0) {
+                ErrorMessage += "height set to a negative value (" + height + ") ";
             }
 
-            if(minDifferent<0){
-                ErrorMessage+="minDifferent set to a negative value ("+minDifferent+") ";
+            if (minDifferent < 0) {
+                ErrorMessage += "minDifferent set to a negative value (" + minDifferent + ") ";
             }
 
-            if(maxDifferent<0){
-                ErrorMessage+="maxDifferent set to a negative value ("+maxDifferent+") ";
-            }
-
-            if(minDifferent>maxDifferent){
-                ErrorMessage+="minDifferent is greater than maxDifferent("+minDifferent+">"+maxDifferent+") ";
+            if (minDifferent > maxDifferent) {
+                ErrorMessage += "minDifferent is greater than maxDifferent(" + minDifferent + ">" + maxDifferent + ") ";
             }
             throw new IllegalArgumentException(ErrorMessage);
         }
@@ -42,7 +40,7 @@ public abstract class Layout {
     }
 
     public String getInfo() {
-        return "-width="+width + " -height=" + height + "-minDifferent=" + minDifferent + "-maxDifferent=" + maxDifferent;
+        return "-width=" + width + " -height=" + height + "-minDifferent=" + minDifferent + "-maxDifferent=" + maxDifferent;
     }
 
     public int getWidth() {
