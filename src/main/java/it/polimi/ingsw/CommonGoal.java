d33e2edcea5465dfedc053e7fc44b69ed65246a5
Matteo Delton <deltonmatteo@gmail.com>
2023-03-25 15:44:21 +0100

    Implement assignScore and fix warnings

--- src/main/java/it/polimi/ingsw/CommonGoal.java
+++ src/main/java/it/polimi/ingsw/CommonGoal.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw;
 
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  * @author Beatrice
  * This class is for the CommonGoal cards
@@ -10,28 +11,28 @@ public class CommonGoal {
     private final Layout layout;
     private final List<Integer> scoringList;
 
-    public CommonGoal(int numofplayers, Layout layout) {
+    public CommonGoal(int numOfPlayers, Layout layout) {
         scoringList = new ArrayList<>();
 
-        if (numofplayers == 2) {
-            /**
+        if (numOfPlayers == 2) {
+            /*
              * i=2 --> 4*i=8
              * i=1 --> 4*i=4
              */
-            for (int i = numofplayers; i > 0; i--) {
+            for (int i = numOfPlayers; i > 0; i--) {
                 scoringList.add(4 * i);
             }
-        } else if (numofplayers == 3) {
-            /**
+        } else if (numOfPlayers == 3) {
+            /*
              * i=3 --> (4*i+4)/2=8
              * i=2 --> (4*i+4)/2=6
              * i=1 --> (4*i+4)/2=4
              */
-            for (int i = numofplayers; i > 0; i--) {
+            for (int i = numOfPlayers; i > 0; i--) {
                 scoringList.add(2 * i + 2);
             }
         } else {
-            for (int i = numofplayers; i > 0; i--) {
+            for (int i = numOfPlayers; i > 0; i--) {
                 scoringList.add(i * 2);
             }
         }
@@ -39,34 +40,32 @@ public class CommonGoal {
         this.layout = layout;
     }
 
-    /*   public int assignScore(Player player){
+    public void assignScore(Player player) {
+        player.setCommonGoalPoints(this);
+    }
 
-       }
-*/
-    public List getScoringList() {
+    public List<Integer> getScoringList() {
         return scoringList;
     }
 
+    // Points depending on the number of players
+    // 2 players: 4, 8
+    // 3 players: 4, 6, 8
+    // 4 players: 2, 4, 6, 8
 
-//points depending on the number of players
-    // 2 players: 4,8
-    // 3 players: 4,6,8
-    //4 players: 2,4,6,8
+    // TODO: check the exception type
 
     /**
-     * @return val, which is the first element of the scoringList and then it is removed
-     * @throws IndexOutOfBoundsException
+     * @return val, which is the first element of the scoringList, and then it is removed
+     * @throws IndexOutOfBoundsException if the list is empty
      * @author Beatrice
      */
-
     public int getScoring() throws IndexOutOfBoundsException {
-
-        if (scoringList.size() > 0) {
-            int val = scoringList.get(0);
-            scoringList.remove(scoringList.get(0));
-            return val;
-        } else
+        if (scoringList.size() == 0)
             throw new IndexOutOfBoundsException();
-    }
 
+        int val = scoringList.get(0);
+        scoringList.remove(scoringList.get(0));
+        return val;
+    }
 }
