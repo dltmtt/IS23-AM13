4199e8f3fa8d472d19791fd1a8fbd5541b2c7960
Matteo Delton <deltonmatteo@gmail.com>
2023-03-30 10:59:34 +0200

    Add javaDoc and comments, use parameters, fix drawCommonGoal
    Parameters are used instead of hard-coding numbers.
    The method drawCommonGoal() draws a number of common goal decided by whether it's any of the players' first game or not.
    Some names have been changed to be more self-explanatory.

--- src/main/java/it/polimi/ingsw/Controllers/Game.java
+++ src/main/java/it/polimi/ingsw/Controllers/Game.java
@@ -1,8 +1,8 @@
 package it.polimi.ingsw.Controllers;
 
-
 import it.polimi.ingsw.Models.CommonGoalLayout.*;
 import it.polimi.ingsw.Models.Game.Board;
+import it.polimi.ingsw.Models.Game.Bookshelf;
 import it.polimi.ingsw.Models.Game.Player;
 import it.polimi.ingsw.Models.Goal.CommonGoal;
 import it.polimi.ingsw.Models.Goal.PersonalGoal;
@@ -13,12 +13,12 @@ import java.util.List;
 import java.util.Random;
 
 public class Game {
+    private static final int personalGoalDeckSize = 12;
     private List<PersonalGoal> personalGoalDeck;
     private List<CommonGoal> commonGoalDeck;
     private Board livingRoom;
-
+    private Bookshelf bookshelf;
     private List<Player> players;
-
     private Player currentPlayer;
 
     public Game(int numOfPlayer) throws IllegalAccessException {
@@ -30,10 +30,9 @@ public class Game {
      *
      * @throws IllegalAccessException if the number of players is invalid
      */
-
     public void initialize(int numOfPlayer) throws IllegalAccessException {
         this.players = new ArrayList<>(numOfPlayer);
-        //TODO: aggiungere i giocatori loggati
+        // TODO: aggiungere i giocatori loggati
 
         // CommonGoalDeck is created and filled
         commonGoalDeck_creation();
@@ -45,88 +44,115 @@ public class Game {
         livingRoom = new Board(players.size());
         livingRoom.fill();
 
-        // draw a personal goal card for each player
+        // Draw a personal goal card for each player
         for (Player player : players) {
-            player.setPersonalGoal(drawPersonalCard());
+            player.setPersonalGoal(drawPersonalGoal());
         }
-        //draw two common goal cards
-        Player.setCommonGoal(drawCommonCard());
 
+        // Draw two common goal cards
+        Player.setCommonGoal(drawCommonGoals());
     }
 
+    // drawPersonalGoal() and drawCommonGoals() are basically the same method,
+    // we could parametrize them and use a single method for both.
+
     /**
-     * This method draws a personal goal card from the personalGoalDeck for the player
+     * Draws a personal goal card from the personalGoalDeck for the player.
      *
      * @return the personal goal card drawn
      */
-    public PersonalGoal drawPersonalCard() {
+    public PersonalGoal drawPersonalGoal() {
         Random randomNumberGenerator = new Random();
-        int number = randomNumberGenerator.nextInt(personalGoalDeck.size());
-        PersonalGoal chosen = personalGoalDeck.get(number);
-        personalGoalDeck.remove(number);
-        return chosen;
-    }
 
-    public List<CommonGoal> drawCommonCard() {
-        Random randomNumberGenerator = new Random();
-        int first = randomNumberGenerator.nextInt(commonGoalDeck.size());
-        List<CommonGoal> chosen = new ArrayList<>();
-        chosen.add(commonGoalDeck.get(first));
-        commonGoalDeck.get(first).setScoringList(players.size());
-        commonGoalDeck.remove(first);
-        int second = randomNumberGenerator.nextInt(commonGoalDeck.size());
-        chosen.add(commonGoalDeck.get(second));
-        commonGoalDeck.get(second).setScoringList(players.size());
-        commonGoalDeck.remove(second);
-        return chosen;
+        int randomPersonalGoalIndex = randomNumberGenerator.nextInt(personalGoalDeck.size());
+        PersonalGoal extracted = personalGoalDeck.get(randomPersonalGoalIndex);
+
+        // Remove the extracted personal goal from the deck so that it can't be drawn again.
+        personalGoalDeck.remove(randomPersonalGoalIndex);
+
+        return extracted;
     }
 
     /**
-     * This method creates the commonGoalDeck
-     * TODO: Aggiungere tutti i layout
+     * Draw the appropriate number of common goal cards from the commonGoalDeck and set their scoring according to the number of players.
+     * The number of common goal cards is 1 if it's the first game for any of the players, otherwise it's 2.
+     *
+     * @return the list of common goal cards drawn
+     */
+    public List<CommonGoal> drawCommonGoals() {
+        Random randomNumberGenerator = new Random();
+        List<CommonGoal> extracted = new ArrayList<>();
+
+        // If it's the first game for any of the players, play with 1 common goal, otherwise play with 2.
+        boolean firstGame = false;
+        for (Player player : players) {
+            if (player.isFirstGame()) {
+                firstGame = true;
+                break;
+            }
+        }
+        int commonGoalNumber = firstGame ? 1 : 2;
+
+        for (int i = 0; i < commonGoalNumber; i++) {
+            int randomLayoutIndex = randomNumberGenerator.nextInt(commonGoalDeck.size());
+            extracted.add(commonGoalDeck.get(randomLayoutIndex));
+            commonGoalDeck.get(randomLayoutIndex).setScoringList(players.size());
+
+            // Remove the extracted common goal from the deck so that it can't be drawn again.
+            commonGoalDeck.remove(randomLayoutIndex);
+        }
+
+        return extracted;
+    }
+
+    // TODO: Add missing layouts
+
+    /**
+     * This method creates the commonGoalDeck.
      */
     public void commonGoalDeck_creation() {
-        //dimension=5->da mettere nel file di configurazione
+        int dimension = Math.min(bookshelf.getRows(), bookshelf.getColumns());
         List<Layout> layouts = new ArrayList<>();
-        layouts.add(new Stair(5));
-        layouts.add(new Diagonal(5, 1, 1));
+
+        // TODO: parametrize the rest of the layouts.
+        layouts.add(new Stair(dimension));
+        layouts.add(new Diagonal(dimension, 1, 1));
         layouts.add(new Corners(1, 1));
         layouts.add(new XShape(3, 1, 1));
-        //columnLine_layout
+        // columnLine_layout
         layouts.add(new FullLine(1, 3, 3, false));
-        //rowLine_layout
+        // rowLine_layout
         layouts.add(new FullLine(1, 3, 4, true));
-        //twoColumnLine_layout
+        // twoColumnLine_layout
         layouts.add(new FullLine(6, 6, 2, false));
-        //twoRowLine_layout
+        // twoRowLine_layout
         layouts.add(new FullLine(5, 5, 2, true));
 
-        //da aggiungere square
-        //da aggiungere rectangle
+        // TODO: aggiungere Square
+        // TODO: aggiungere Rectangle
 
         for (Layout layout : layouts) {
             commonGoalDeck.add(new CommonGoal(layout));
         }
-
     }
 
     /**
      * This method creates the personalGoalDeck
      */
     public void personalGoalDeck_creation() {
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < personalGoalDeckSize; i++) {
             // Add a new personal goal in the List
             personalGoalDeck.add(new PersonalGoal(i));
         }
     }
 
     /**
-     * This is the move method, it calls the move method of the player
+     * This method move a column or a row of items from the board to the player's bookshelf.
      *
-     * @param player
-     * @param from
-     * @param to
-     * @param column
+     * @param player the player who wants to move the items
+     * @param from   the starting board's coordinates of the items to move
+     * @param to     the final board's coordinates of the items to move
+     * @param column the index of the bookshelf's column where the items will be moved
      */
     public void move(Player player, Coordinates from, Coordinates to, int column) {
         player.move(from, to, column);
