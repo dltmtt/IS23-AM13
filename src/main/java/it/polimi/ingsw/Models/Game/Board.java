0e9942df948c5eb01424d098649f97442021489f
Matteo Delton <deltonmatteo@gmail.com>
2023-03-30 13:39:39 +0200

    Generalize code

--- src/main/java/it/polimi/ingsw/Models/Game/Board.java
+++ src/main/java/it/polimi/ingsw/Models/Game/Board.java
@@ -10,47 +10,26 @@ import java.util.List;
 import java.util.Random;
 
 public class Board {
-    // Matrix 9x9
+    private static final int boardSize = 9;
+    private static final int numOfColorOccurrences = 22;
     private final Item[][] boardMatrix;
     private final List<Item> itemBag;
     private final UsableCells usableCells;
-
     private final int numOfPlayer;
 
 
     public Board(int numOfPlayer) {
-        boardMatrix = new Item[9][9];
+        boardMatrix = new Item[boardSize][boardSize];
         usableCells = new UsableCells(numOfPlayer);
         itemBag = new ArrayList<>();
         this.numOfPlayer = numOfPlayer;
 
-        /*
-         * Bag of Item initialized
-         * i % 3 -> for the 3 possible images for each color, it is only used for the view.
-         * i < 22 -> 'cause each color has 22 occurrences
-         * At the end we have a list with 132 elements (Item)
-         */
-        for (int i = 0; i < 22; i++) {
-            itemBag.add(new Item(Color.GREEN, i % 3));
+        // Initialize the bag of items. For each color, there are 22 occurrences with 3 different images for a total of 132 items.
+        for (int i = 0; i < numOfColorOccurrences; i++) {
+            for (Color color : Color.values()) {
+                itemBag.add(new Item(color, i % 3));
+            }
         }
-
-        for (int i = 0; i < 22; i++) {
-            itemBag.add(new Item(Color.PINK, i % 3));
-        }
-        for (int i = 0; i < 22; i++) {
-            itemBag.add(new Item(Color.WHITE, i % 3));
-        }
-        for (int i = 0; i < 22; i++) {
-            itemBag.add(new Item(Color.YELLOW, i % 3));
-        }
-        for (int i = 0; i < 22; i++) {
-            itemBag.add(new Item(Color.BLUE, i % 3));
-        }
-        for (int i = 0; i < 22; i++) {
-            itemBag.add(new Item(Color.LIGHTBLUE, i % 3));
-        }
-
-
     }
 
     public List<Item> getItemBag() {
@@ -61,11 +40,10 @@ public class Board {
         return boardMatrix[row][column];
     }
 
-
     public void fill() throws IllegalAccessException {
         Random randNumberGenerator = new Random();
-        for (int row = 0; row < 9; row++) {
-            for (int column = 0; column < 9; column++) {
+        for (int row = 0; row < boardSize; row++) {
+            for (int column = 0; column < boardSize; column++) {
                 if (itemBag.isEmpty()) {
                     throw new IllegalAccessException("the list is empty");
                 } else if (usableCells.getList().contains(new Coordinates(row, column))) {
@@ -73,9 +51,7 @@ public class Board {
                     boardMatrix[row][column] = itemBag.get(indexRandom);
                     itemBag.remove(indexRandom);
                 }
-
             }
-
         }
     }
 
