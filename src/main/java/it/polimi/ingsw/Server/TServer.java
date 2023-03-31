6ebfe4cdcbd590681e05872059c6d421b8cf8e40
Beatrice Di Raimondo Metallo <beatrice01@alice.it>
2023-03-31 23:54:20 +0200

    Added new class ClientHandler and TServer

---
+++ src/main/java/it/polimi/ingsw/Server/TServer.java
@@ -0,0 +1,50 @@
+package it.polimi.ingsw.Server;
+
+import it.polimi.ingsw.Client.ClientHandler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TServer {
+    private final int portNumber;
+    ServerSocket serverSocket = null;
+
+    public TServer(int port) {
+        this.portNumber = port;
+    }
+
+    public static void main(String[] args) {
+        TServer echoServer = new TServer(1234);
+        echoServer.run();
+    }
+
+    public void run() {
+        //it creates new Threads when it's necessary,
+        //but it uses the existing ones until it's possible to
+        ExecutorService ex = Executors.newCachedThreadPool();
+
+        //opening a new socket
+        try {
+            serverSocket = new ServerSocket(portNumber);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //if here, the server is ready
+        System.out.println("Server is ready");
+        while (true) {
+            try {
+                //accepting the connection, it possible
+                assert serverSocket != null;
+                Socket socket = serverSocket.accept();
+                ex.submit(new ClientHandler(socket));
+            } catch (IOException e) {
+                break;
+            }
+        }
+        ex.shutdown();
+    }
+}
