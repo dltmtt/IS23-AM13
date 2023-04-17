6d4b57f30202aac3e6bd76af5504c10f18328a77
Valer0109 <valeria.degennaro@mail.polimi.it>
2023-04-17 13:41:48 +0200

    personal goal json TEST

---
+++ src/test/java/it/polimi/ingsw/JsonPersonalGoalTest.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw;
+
+
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+import org.junit.jupiter.api.Test;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class JsonPersonalGoalTest {
+    @Test
+    public void test() throws FileNotFoundException {
+    }
+}
