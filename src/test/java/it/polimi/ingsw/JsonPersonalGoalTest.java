ad5dcb000b2bbe0ccbecd10f59c07ce50536b7cd
Simone Corbo <simone.corbo@mail.polimi.it>
2023-04-17 13:45:02 +0200

    add JSON example
    bye bye

--- src/test/java/it/polimi/ingsw/JsonPersonalGoalTest.java
+++ src/test/java/it/polimi/ingsw/JsonPersonalGoalTest.java
@@ -1,17 +1,39 @@
 package it.polimi.ingsw;
 
 
-
-import com.google.gson.JsonArray;
-import com.google.gson.JsonParser;
-import org.junit.jupiter.api.Test;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class JsonPersonalGoalTest {
     @Test
-    public void test() throws FileNotFoundException {
+    public void test() throws IOException, ParseException {
+
+        JSONParser parser = new JSONParser();
+        JSONArray a = (JSONArray) parser.parse(new FileReader("src/main/resources/PersonalGoal.json"));
+
+        for (Object o : a) {
+            JSONObject person = (JSONObject) o;
+
+            String name = (String) person.get("name");
+            System.out.println(name);
+
+            String city = (String) person.get("city");
+            System.out.println(city);
+
+            String job = (String) person.get("job");
+            System.out.println(job);
+
+            JSONArray cars = (JSONArray) person.get("cars");
+
+            for (Object c : cars) {
+                System.out.println(c);
+            }
+        }
     }
 }
