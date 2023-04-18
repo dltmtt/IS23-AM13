f4fa9dd145d2d9134ba67ee8a139bfff0d542307
Matteo Delton <deltonmatteo@gmail.com>
2023-04-18 15:20:24 +0200

    Add hex colors

--- src/main/java/it/polimi/ingsw/utils/Color.java
+++ src/main/java/it/polimi/ingsw/utils/Color.java
@@ -4,23 +4,39 @@ import java.util.Random;
 
 /**
  * The color of an item. It identifies the different types of items.
+ * Each color has a different hex color code.
  */
 public enum Color {
-    GREEN,
-    WHITE,
-    YELLOW,
-    BLUE,
-    LIGHTBLUE,
-    PINK;
+    GREEN("#8e9d47"),
+    WHITE("#ebe1bf"),
+    YELLOW("#e1a546"),
+    BLUE("#00648d"),
+    LIGHTBLUE("#62b2b1"),
+    PINK("#c64b79");
+
+    private final String hexCode;
+
+    Color(String hexColorCode) {
+        this.hexCode = hexColorCode;
+    }
 
     /**
-     * Picks a random color from the available ones.
-     *
      * @return a random color
      */
     public static Color getRandomColor() {
-        Random rnd = new Random();
-        Color[] colors = values();
-        return colors[rnd.nextInt(colors.length)];
+        Random random = new Random();
+        Color[] colors = Color.values();
+        return colors[random.nextInt(colors.length)];
+    }
+
+    /**
+     * @return the hex color code of a random color
+     */
+    public static String getRandomHexColor() {
+        return getRandomColor().getHexColorCode();
+    }
+
+    public String getHexColorCode() {
+        return hexCode;
     }
 }
