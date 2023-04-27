b0a63b33820a8ef54d2f0f3f624592d7a73c16bf
Simone Corbo <simone.corbo@mail.polimi.it>
2023-04-27 14:03:09 +0200

    first socket implementation
    Co-Authored-By: BeaDRM <126068578+BeatriceDRM@users.noreply.github.com>

--- src/main/java/it/polimi/ingsw/server/socket/TCPServer.java
+++ src/main/java/it/polimi/ingsw/server/socket/TCPServer.java
@@ -1,76 +1,106 @@
 package it.polimi.ingsw.server.socket;
 
-import it.polimi.ingsw.client.ClientController;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-public class TCPServer {
-    private static final int minAge = 8;
-    private final int portNumber;
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-
-    public TCPServer(int port) {
-        this.portNumber = port;
-    }
-
+// Server class
+class TCPServer {
     public static void main(String[] args) {
-        TCPServer echoServer = new TCPServer(1234);
-        echoServer.run();
-    }
-
-    public void run() {
-        // ExecutorService Creates new threads when it's necessary,
-        // but uses the existing ones when possible.
-        ExecutorService ex = Executors.newCachedThreadPool();
+        ServerSocket server = null;
 
         try {
-            serverSocket = new ServerSocket(portNumber);
+
+            // server is listening on port 1234
+            server = new ServerSocket(1234);
+            server.setReuseAddress(true);
+
+            // running infinite loop for getting
+            // client request
+            while (true) {
+
+                // socket object to receive incoming client
+                // requests
+                Socket client = server.accept();
+
+                // Displaying that new client is connected
+                // to server
+                System.out.println("New client connected"
+                        + client.getInetAddress()
+                        .getHostAddress());
+
+                // create a new thread object
+                ClientHandler clientSock
+                        = new ClientHandler(client);
+
+                // This thread will handle the client
+                // separately
+                new Thread(clientSock).start();
+            }
         } catch (IOException e) {
             e.printStackTrace();
-        }
-
-        // If here, the server is ready
-        System.out.println("The server is ready");
-
-        while (true) {
-            try {
-                // Accepting the connection, if possible
-                assert serverSocket != null;
-                clientSocket = serverSocket.accept();
-                ex.submit(new ClientController(clientSocket));
+        } finally {
+            if (server != null) {
                 try {
-                    log();
-                } catch (Exception e) {
+                    server.close();
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
-            } catch (IOException e) {
-                break;
             }
         }
-        ex.shutdown();
     }
 
-    public void log() throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        PrintWriter out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+    // ClientHandler class
+    private static class ClientHandler implements Runnable {
+        private final Socket clientSocket;
 
-        String name;
-        name = in.readLine();
-        out.println("The name is: " + name);
-
-        int age;
-        age = in.read();
-        if (age < minAge) {
-            out.println("The player " + name + " must be accompanied by an adult to play.");
+        // Constructor
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
         }
 
-        in.close();
-        out.flush();
-        out.close();
+        public void run() {
+            PrintWriter out = null;
+            BufferedReader in = null;
+            try {
+
+                // get the outputstream of client
+                out = new PrintWriter(
+                        clientSocket.getOutputStream(), true);
+
+                // get the inputstream of client
+                in = new BufferedReader(
+                        new InputStreamReader(
+                                clientSocket.getInputStream()));
+
+                String line;
+                while ((line = in.readLine()) != null) {
+
+                    // writing the received message from
+                    // client
+                    System.out.printf(
+                            " Sent from the client: %s\n",
+                            line);
+                    out.println(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (out != null) {
+                        out.close();
+                    }
+                    if (in != null) {
+                        in.close();
+                        clientSocket.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
