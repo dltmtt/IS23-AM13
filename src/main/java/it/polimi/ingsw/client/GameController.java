f7abfe585af40a018174049596bdb7ac91d15384
Matteo Delton <deltonmatteo@gmail.com>
2023-04-30 12:19:19 +0200

    Add game controller

---
+++ src/main/java/it/polimi/ingsw/client/GameController.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.client;
+
+import it.polimi.ingsw.client.view.GameView;
+import it.polimi.ingsw.server.model.GameModel;
+
+public class GameController {
+    private final GameModel gameModel;
+    private final GameView gameView;
+    private final Client client;
+
+    public GameController(GameModel gameModel, GameView gameView, Client client) {
+        this.gameModel = gameModel;
+        this.gameView = gameView;
+        this.client = client;
+        gameView.setController(this);
+    }
+
+    public void startGame() {
+        gameModel.start();
+    }
+
+    public void login(String username) throws Exception {
+        client.login(username);
+    }
+
+    public void showLoginScreen() {
+        gameView.showLogin();
+    }
+}
