553e32f7229ac7d8bed589aff217d48824e83beb
Matteo Delton <deltonmatteo@gmail.com>
2023-05-03 20:30:20 +0200

    Separate RMI and socket server creation
    If a server starts to fails, the server as a whole starts anyway

--- src/main/java/it/polimi/ingsw/server/Server.java
+++ src/main/java/it/polimi/ingsw/server/Server.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.server;
 import it.polimi.ingsw.commons.CommunicationInterface;
 import it.polimi.ingsw.commons.Message;
 import it.polimi.ingsw.server.model.GameModel;
+import it.polimi.ingsw.server.socket.MultiEchoServer;
 import it.polimi.ingsw.utils.FullRoomException;
 
 import java.io.BufferedReader;
@@ -22,20 +23,26 @@ import static it.polimi.ingsw.utils.CliUtilities.RESET;
  */
 public class Server implements ServerInterface, CommunicationInterface {
     private static final String SHUTDOWN_COMMAND = "exit";
-    private final ServerRmi rmiServer;
-    //    private final MultiEchoServer socketServer;
     private final ServerParser parser = new ServerParser();
     private final ServerController controller = new ServerController();
+    private ServerRmi rmiServer;
+    private MultiEchoServer socketServer;
     private GameModel gameModel = null;
 
     public Server() {
 //        this.gameModel = new GameModel();
         try {
             this.rmiServer = new ServerRmi(this.gameModel);
-//            this.socketServer = new MultiEchoServer();
         } catch (RemoteException e) {
             e.printStackTrace();
-            throw new RuntimeException("Unable to create RMI server.");
+            System.err.println("Unable to create RMI server.");
+        }
+
+        try {
+            this.socketServer = new MultiEchoServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Unable to create socket server.");
         }
     }
 
@@ -71,7 +78,7 @@ public class Server implements ServerInterface, CommunicationInterface {
     public void start() {
         try {
             rmiServer.start();
-//            socketServer.start();
+            socketServer.start();
         } catch (RemoteException e) {
             e.printStackTrace();
             System.exit(1);
@@ -84,7 +91,7 @@ public class Server implements ServerInterface, CommunicationInterface {
         System.out.println("Shutting down...");
         try {
             rmiServer.stop();
-//            socketServer.stop();
+            socketServer.stop();
         } catch (RemoteException | NotBoundException e) {
             e.printStackTrace();
         }
