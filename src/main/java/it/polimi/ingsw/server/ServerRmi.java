d56953eb84f57c584fb43ef6a1fa3ee703c460ff
Matteo Delton <deltonmatteo@gmail.com>
2023-05-04 02:33:41 +0200

    Fix servers and delete separate TCP shutdown command handling
    The shutdown command is handled by a thread in the Server class

--- src/main/java/it/polimi/ingsw/server/ServerRmi.java
+++ src/main/java/it/polimi/ingsw/server/ServerRmi.java
@@ -1,46 +1,34 @@
 package it.polimi.ingsw.server;
 
-import it.polimi.ingsw.commons.CommunicationInterface;
-import it.polimi.ingsw.server.model.GameModel;
-
+import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-import static it.polimi.ingsw.server.Server.PORT_RMI;
-
-public class ServerRmi implements ServerInterface {
-    private final GameModel model;
+public class ServerRmi implements CommunicationInterface {
     private Registry registry;
     private CommunicationInterface server;
     private CommunicationInterface stub;
 
-    public ServerRmi(GameModel model) throws RemoteException {
-        this.model = model;
+    public ServerRmi() throws RemoteException {
+        super();
     }
 
-    @Override
-    public void start() throws RemoteException {
-        server = new Server();
+    public void start() throws RemoteException, MalformedURLException {
+        server = new ServerRmi();
         stub = (CommunicationInterface) UnicastRemoteObject.exportObject(server, 0);
 
         registry = LocateRegistry.createRegistry(PORT_RMI);
         registry.rebind("CommunicationInterface", stub);
 
-        System.out.println("RMI server configuration completed.");
+        System.out.println("Server RMI started on port " + PORT_RMI + ".");
     }
 
-    @Override
     public void stop() throws RemoteException, NotBoundException {
         registry.unbind("CommunicationInterface");
         UnicastRemoteObject.unexportObject(server, true);
         System.out.println("RMI server stopped.");
     }
-
-//    @Override
-//    public String sendMessage(String clientMessage) throws RemoteException {
-//
-//    }
 }
