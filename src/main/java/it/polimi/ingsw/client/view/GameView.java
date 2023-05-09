3cf9b9c3ec2773ce08b080e83292e060534e9c14
Matteo Delton <deltonmatteo@gmail.com>
2023-05-09 23:30:11 +0200

    Remove extra space

--- src/main/java/it/polimi/ingsw/client/view/GameView.java
+++ src/main/java/it/polimi/ingsw/client/view/GameView.java
@@ -14,7 +14,7 @@ public abstract class GameView {
 
     protected final String insertUsernamePrompt = "Please, insert your username: ";
     protected final String insertAgePrompt = "Please, insert your age: ";
-    protected final String firstGamePrompt = "Is this the first time you play this game? ";
+    protected final String firstGameQuestion = "Is this the first time you play this game?";
     protected final String insertUsernameAgainPrompt = "Please, insert your username again: ";
     protected final String welcomeMessage = "Welcome to My Shelfie!\n";
     protected final String insertNumberOfPlayersPrompt = "Please, insert the number of players you want to play with: ";
