1a93fe63a17da335700bda7ad23dc28523bfe664
Valer0109 <valeria.degennaro@mail.polimi.it>
2023-06-28 10:12:28 +0200

    add some PlayerTest

--- src/test/java/it/polimi/ingsw/PlayerTest.java
+++ src/test/java/it/polimi/ingsw/PlayerTest.java
@@ -8,8 +8,11 @@ import it.polimi.ingsw.server.model.layouts.Diagonal;
 import it.polimi.ingsw.server.model.layouts.Layout;
 import it.polimi.ingsw.server.model.layouts.Stair;
 import it.polimi.ingsw.utils.Color;
+import it.polimi.ingsw.utils.SettingLoader;
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,17 +36,17 @@ public class PlayerTest {
     }
 
     @Test
-    void getCommonGoalNames(){
-        List <Boolean> commons = new ArrayList<>();
-        List <CommonGoal> commonGoal = new ArrayList<>();
+    void getCommonGoalNames() {
+        List<Boolean> commons = new ArrayList<>();
+        List<CommonGoal> commonGoal = new ArrayList<>();
         commons.add(true);
         commons.add(false);
 
         Player player = new Player("test", 0, true, true, true, commons);
         Layout s = new Diagonal(1, 3, 2);
         Layout s2 = new Stair(1, 6, Bookshelf.getColumns());
-        commonGoal.add(new CommonGoal(s,2));
-        commonGoal.add(new CommonGoal(s2,2));
+        commonGoal.add(new CommonGoal(s, 2));
+        commonGoal.add(new CommonGoal(s2, 2));
         player.setCommonGoals(commonGoal);
 
         assertEquals("diagonal", player.getCommonNames().get(0));
@@ -52,11 +55,31 @@ public class PlayerTest {
     }
 
     @Test
-    void setPlayer(){
+    void setPlayer() {
         Player p = new Player("Ciao", 0, true, true, true);
         p.setIsFirstPlayer(true);
         assertTrue(p.isFirstPlayer());
-        assertEquals("Ciao",p.getNickname());
+        assertEquals("Ciao", p.getNickname());
         p.setHasEndGameCard(true);
     }
+
+    @Test
+    void set_get_points() throws IOException, ParseException {
+        Player p = new Player("Ciao", 0, true, true, true);
+        List<CommonGoal> commonGoals = new ArrayList<>();
+        commonGoals.add(new CommonGoal(new Diagonal(1, 3, 2), 2));
+        commonGoals.add(new CommonGoal(new Stair(1, 6, Bookshelf.getColumns()), 2));
+        p.setCommonGoals(commonGoals);
+        List<Integer> points = new ArrayList<>();
+        points.add(4);
+        points.add(8);
+        p.setCommonGoalPoints(points);
+        assertEquals(12, p.getCommonGoalPoints());
+        p.setPersonalGoal(SettingLoader.loadSpecificPersonalGoal(1));
+        Bookshelf b = new Bookshelf(6, 5);
+        p.setBookshelf(b);
+        assertEquals(0, p.getPersonalGoalPoints());
+    }
+
+
 }
